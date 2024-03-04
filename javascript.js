3cab1157fbb2fcf4562f71bffe29c511c0a7abd0
dizmerly <dan.izmerly@gmail.com>
2024-03-04 12:30:28 -0600

    Added data targets for buttons to get operator status from button

--- javascript.js
+++ javascript.js
@@ -1,8 +1,28 @@
 const body = document.querySelector(".bodyCalc");
 const buttons = document.querySelectorAll(".btn");
+const screen = document.querySelector(".screen")
 
 
+buttons.forEach((button) => 
+{
+    button.addEventListener('click', (event) => 
+    {   
+        if(button.className.indexOf("number") != -1)
+        {
+            screen.textContent = screen.textContent.push(button.textContent);
+        }
+        else if(button.className.indexOf("operator") != -1)
+        {
+            console.log(button.getAttribute("data-target"));
+        }
 
-buttons.forEach((button) => {
-    button.addEventListener('click', (event) => console.log("Clicked!"));
+
+        else
+        {
+            console.log("Clicked " + button.textContent);
+        }        
+    });
 }); 
+
+screen.textContent = "02313";
+
