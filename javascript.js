b0edb0182ad23ec5f2a3ee3531381b63249e6d83
dizmerly <dan.izmerly@gmail.com>
2024-03-09 00:54:17 -0600

    t "New commi:wq "

--- javascript.js
+++ javascript.js
@@ -1,7 +1,22 @@
+import { create, all } from 'mathjs'
+
+
+
 const body = document.querySelector(".bodyCalc");
 const buttons = document.querySelectorAll(".btn");
 const screen = document.querySelector(".screen")
 
+let equation = ""; 
+
+
+let updateContent = (destination, content) => {
+    let tempText = destination.textContent + content;
+    destination.textContent = tempText;
+};
+
+let clearContent = (destination) => {
+    destination.textContent = "";
+};
 
 buttons.forEach((button) => 
 {
@@ -9,11 +24,15 @@ buttons.forEach((button) =>
     {   
         if(button.className.indexOf("number") != -1)
         {
-            screen.textContent = screen.textContent.push(button.textContent);
+            
+            updateContent(screen, button.textContent);
         }
         else if(button.className.indexOf("operator") != -1)
         {
             console.log(button.getAttribute("data-target"));
+        }
+        else if(button.getAttribute('data-target') == "clear"){
+            clearContent(screen);
         }
 
 
@@ -24,5 +43,6 @@ buttons.forEach((button) =>
     });
 }); 
 
-screen.textContent = "02313";
+
+console.log(math.evaluate("2+2"));
 
