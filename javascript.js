89d964cc9cc7877ed3d75b6dd9bdd6fcf3194bdd
dizmerly <dan.izmerly@gmail.com>
2024-03-12 23:35:02 -0500

    Added evaluation functions for calculator

--- javascript.js
+++ javascript.js
@@ -4,14 +4,21 @@ const screen = document.querySelector(".screen")
 
 let equation = ""; 
 
-
 let updateContent = (destination, content) => {
-    let tempText = destination.textContent + content;
+    if((destination.textContent).length < 9){
+     let tempText = destination.textContent + content;
     destination.textContent = tempText;
+    };
+   
 };
 
 let clearContent = (destination) => {
     destination.textContent = "";
+};
+
+let clearSelection = (elements) => {
+    
+    Array.from(elements).forEach((element) => element.classList.remove("selected"));
 };
 
 buttons.forEach((button) => 
@@ -20,24 +27,36 @@ buttons.forEach((button) =>
     {   
         if(button.className.indexOf("number") != -1)
         {
-            
             updateContent(screen, button.textContent);
+            equation += screen.textContent;
+
         }
         else if(button.className.indexOf("operator") != -1)
         {
-            console.log(button.getAttribute("data-target"));
-        }
-        else if(button.getAttribute('data-target') == "clear"){
+            equation += button.getAttribute('data-target');
+            button.classList.add("selected");
             clearContent(screen);
         }
-
+        else if(button.getAttribute('data-target') == "clear")
+        {
+            clearContent(screen);
+            clearSelection(document.getElementsByClassName("selected"));
+        }
+        else if(button.getAttribute('data-target') == "equals")
+        {
+            clearContent(screen);
+            updateContent(screen, math.evaluate(equation));
+            equation = "";
+            clearSelection(document.getElementsByClassName("selected"));
+            
+            
+        }
 
         else
         {
             console.log("Clicked " + button.textContent);
-        }        
+        }
+
     });
 }); 
-
-
 
